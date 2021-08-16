c19db552ec9b0bc13b59e214287361f863d88a13
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-15 23:02:43 -0500

    added additional app.get to pull one animal based on the id

--- server.js
+++ server.js
@@ -47,6 +47,10 @@ function filterByQuery(query, animalsArray) {
   // return the filtered results:
   return filteredResults;
 }
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -56,4 +60,12 @@ app.get("/api/animals", (req, res) => {
 });
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
+});
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
